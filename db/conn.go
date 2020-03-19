99112dec3dce43bf8ce4cb989f962eca3c0b3178
Dushyant Yadav <dushyant9309@gmail.com>
2020-03-20 01:32:04 +0530

    Create structures to manage database connection

---
+++ db/conn.go
@@ -0,0 +1,27 @@
+package db
+
+import (
+	"log"
+
+	"github.com/neo4j/neo4j-go-driver/neo4j"
+)
+
+// Conn holds connection information to the database
+type Conn struct {
+	URI    string
+	Driver *neo4j.Driver
+}
+
+// QueryContext is the data type meant to be passed in
+// neo4j transactions for dynamic queries
+type QueryContext map[string]interface{}
+
+// Initialize actually connects to the database
+// and store driver information in the connection struct
+func (c *Conn) Initialize(username string, password string) {
+	driver, err := neo4j.NewDriver(c.URI, neo4j.BasicAuth(username, password, ""))
+	if err != nil {
+		log.Fatal("Unable to connect to database:", err)
+	}
+	(*c).Driver = &driver
+}
