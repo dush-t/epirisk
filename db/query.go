98657200f225fdcf111da766a7bf7463d2ce22be
Dushyant Yadav <dushyant9309@gmail.com>
2020-03-20 01:31:29 +0530

    Add query to get user by phone number

--- db/query.go
+++ db/query.go
@@ -1,21 +1,16 @@
-package main
+package db
 
 import (
 	"log"
 
 	"github.com/dush-t/epirisk/db/models"
+	"github.com/dush-t/epirisk/util"
 	"github.com/neo4j/neo4j-go-driver/neo4j"
 )
 
-// Conn stores information about the current database connection
-type Conn struct {
-	uri    string
-	driver *neo4j.Driver
-}
-
 // AddUser adds a new user to the database
-func (db Conn) AddUser(phoneNo string, name string) (interface{}, error) {
-	driver := *(db.driver)
+func AddUser(c Conn, phoneNo string, password string, name string) (models.User, error) {
+	driver := *(c.Driver)
 	session, err := driver.Session(neo4j.AccessModeWrite)
 	if err != nil {
 		log.Fatal("Failed to connect to database")
@@ -23,12 +18,15 @@ func (db Conn) AddUser(phoneNo string, name string) (interface{}, error) {
 	}
 	defer session.Close()
 
+	passwordHash, _ := util.HashPassword(password)
+
 	user, err := session.WriteTransaction(func(transaction neo4j.Transaction) (interface{}, error) {
 		result, err := transaction.Run(
-			"CREATE (user:User) SET user.PhoneNo = $phoneNo SET user.Name = $name SET user.Risk=0.0 RETURN user",
-			map[string]interface{}{
-				"phoneNo": phoneNo,
-				"name":    name,
+			"CREATE (user:User) SET user.PhoneNo = $phoneNo SET user.Password = $password SET user.Name = $name SET user.Risk=0.0 RETURN user",
+			QueryContext{
+				"phoneNo":  phoneNo,
+				"password": passwordHash,
+				"name":     name,
 			})
 		if err != nil {
 			log.Fatal(err)
@@ -47,8 +45,46 @@ func (db Conn) AddUser(phoneNo string, name string) (interface{}, error) {
 		return models.User{}, err
 	}
 
-	userDataMap := (user.(neo4j.Node)).Props()
-	userEntity := models.BuildUserFromMap(userDataMap)
+	userEntity := models.GetUserFromNode(user.(neo4j.Node))
+
+	return userEntity, nil
+}
+
+// GetUser queries the database and gets a user by phoneNo.
+func GetUser(c Conn, phoneNo string) (models.User, error) {
+	driver := *(c.Driver)
+	session, err := driver.Session(neo4j.AccessModeRead)
+	if err != nil {
+		log.Fatal("Failed to connect to database")
+		return models.User{}, err
+	}
+	defer session.Close()
+
+	user, err := session.ReadTransaction(func(transaction neo4j.Transaction) (interface{}, error) {
+		result, err := transaction.Run(
+			"MATCH (user:User {PhoneNo: $phoneNo}) RETURN user",
+			QueryContext{
+				"phoneNo": phoneNo,
+			},
+		)
+		if err != nil {
+			log.Fatal(err)
+			return nil, err
+		}
+
+		if result.Next() {
+			return result.Record().GetByIndex(0), nil
+		}
+
+		return nil, result.Err()
+	})
+
+	if err != nil {
+		log.Fatal(err)
+		return models.User{}, err
+	}
+
+	userEntity := models.GetUserFromNode(user.(neo4j.Node))
 
 	return userEntity, nil
 }
