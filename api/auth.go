70d5a0e2c28dafcd3ba2c28dea473de347a2fb50
Dushyant Yadav <dushyant9309@gmail.com>
2020-03-20 01:32:59 +0530

    Sign in and sign up works

---
+++ api/auth.go
@@ -0,0 +1,94 @@
+package api
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+
+	"github.com/dgrijalva/jwt-go"
+	"github.com/dush-t/epirisk/db"
+	"github.com/dush-t/epirisk/util"
+)
+
+// Claims stores the data that will be encoded in a user's JWT
+type Claims struct {
+	PhoneNo string `json:"phoneNo"`
+	jwt.StandardClaims
+}
+
+// SignInHandler is the handler function for requests at /sign_in
+func SignInHandler(c db.Conn) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// Decode request body
+		var data struct {
+			PhoneNo  string `json:"phoneNo"`
+			Password string `json:"password"`
+		}
+		err := json.NewDecoder(r.Body).Decode(&data)
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+
+		user, err := db.GetUser(c, data.PhoneNo)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+
+		if !util.MatchesWithHash(data.Password, user.Password) {
+			w.WriteHeader(http.StatusUnauthorized)
+			return
+		}
+
+		tokenString, err := user.GenerateJWT()
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		payload := struct {
+			Token string `json:"token"`
+		}{Token: tokenString}
+		json.NewEncoder(w).Encode(payload)
+	})
+}
+
+// SignUpHandler is the handler function for requests at /sign_up
+func SignUpHandler(c db.Conn) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		var data struct {
+			PhoneNo  string `json:"phoneNo"`
+			Password string `json:"password"`
+			Name     string `json:"name"`
+		}
+		err := json.NewDecoder(r.Body).Decode(&data)
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+
+		user, err := db.AddUser(c, data.PhoneNo, data.Password, data.Name)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			log.Println("Error creating user:", err)
+			return
+		}
+
+		tokenString, err := user.GenerateJWT()
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			log.Println("Error generating token for user:", err)
+			return
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusCreated)
+		payload := struct {
+			Token string `json:"token"`
+		}{Token: tokenString}
+		json.NewEncoder(w).Encode(payload)
+	})
+}
--- db/models/user.go
+++ db/models/user.go
@@ -1,19 +1,51 @@
 package models
 
+import (
+	"time"
+
+	"github.com/dgrijalva/jwt-go"
+	"github.com/neo4j/neo4j-go-driver/neo4j"
+)
+
 // User represents the User entity stored in the database
 type User struct {
-	PhoneNo string
-	Name    string
-	Risk    float64
+	PhoneNo  string
+	Password string
+	Name     string
+	Risk     float64
 }
 
-// BuildUserFromMap accepts a map containing user data
-// and returns a User struct. It's just a utility function.
-func BuildUserFromMap(u map[string]interface{}) User {
+type claims struct {
+	PhoneNo string `json:"phoneNo"`
+	jwt.StandardClaims
+}
+
+// GenerateJWT generates a jwt for a user
+func (u User) GenerateJWT() (string, error) {
+	expirationTime := time.Now().Add(60 * 24 * time.Hour)
+	claims := &claims{
+		PhoneNo: u.PhoneNo,
+		StandardClaims: jwt.StandardClaims{
+			ExpiresAt: expirationTime.Unix(),
+		},
+	}
+	jwtKey := []byte("lolmao12345")
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	tokenString, err := token.SignedString(jwtKey)
+	if err != nil {
+		return "", err
+	}
+	return tokenString, nil
+}
+
+// GetUserFromNode converts a neo4j node to a User Struct
+func GetUserFromNode(n neo4j.Node) User {
+	u := n.Props()
 	user := User{
-		PhoneNo: u["PhoneNo"].(string),
-		Name:    u["Name"].(string),
-		Risk:    u["Risk"].(float64),
+		PhoneNo:  u["PhoneNo"].(string),
+		Password: u["Password"].(string),
+		Name:     u["Name"].(string),
+		Risk:     u["Risk"].(float64),
 	}
 
 	return user
