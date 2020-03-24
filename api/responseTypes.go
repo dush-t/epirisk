170f1f7d6a60c08f3662ca2c6b0f85072ad4d097
Dushyant Yadav <dushyant9309@gmail.com>
2020-03-24 15:25:26 +0530

    Remove risk responseTypes

--- api/responseTypes.go
+++ api/responseTypes.go
@@ -6,25 +6,13 @@ import "github.com/dush-t/epirisk/db/models"
 type UserResponse struct {
 	PhoneNo      string
 	Name         string
-	Risk         float64
 	HealthStatus float64
-	Infected     bool
-}
-
-// RiskDetailsResponse is used to build the JSON response for score
-// details in http handlers
-type RiskDetailsResponse struct {
-	Risk                float64
-	AverageRisk         float64
-	AverageRiskGoodness float64
 }
 
 func buildResponseFromUser(u models.User) UserResponse {
 	return UserResponse{
 		PhoneNo:      u.PhoneNo,
 		Name:         u.Name,
-		Risk:         u.Risk,
 		HealthStatus: u.HealthStatus,
-		Infected:     u.Infected,
 	}
 }
---
+++ constants/constants.go
@@ -0,0 +1,5 @@
+package constants
+
+// FeelingSymptomsHealthStatus is value of HealthStatus
+// associated with feeling symptoms
+var FeelingSymptomsHealthStatus float64 = 0.9
